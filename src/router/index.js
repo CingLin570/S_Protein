7d6ca314d1aa8baa897057ca1db198d0b6b4bfd7
謦麟 <asd28618371@gmail.com>
2020-10-06 15:19:50 +0800

    加入 切換後網頁保持最上方

--- src/router/index.js
+++ src/router/index.js
@@ -107,7 +107,13 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return { x: 0, y: 0 }
+  }
 })
 
 export default router
